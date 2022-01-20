8bc1e1fdc0094a68a12d860674cc5ab71425dd45
Linda LW <lindalw03@gmail.com>
2022-01-20 00:15:36 -0500

    added projects and images

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -6,8 +6,28 @@ export default function About() {
     return (
         	<div class="container mt-5">
 			<h1 className="text-end px-5 mb-5 bg-danger text-white py-3">About</h1>
-            <img className="mx-auto" src= {banner} width='100%' height='25%' alt='hotairballoons'></img>
-      <p className='px-5'>
+            <img className="mx-auto py-e" src= {banner} width='100%' height='25%' alt='hotairballoons'></img>
+            <div className="card text-center py-3">
+      <div class="card-body">
+    
+        <p class="card-text">
+        <p>Based in beautiful Hunterdon County NJ, I have recently completed a Rutgers University Coding Bootcamp to become a Full Stack
+            Developer. I was previously a C/C++/Unix developer for over 15 years and I am excited to be learning the latest technologies. </p>
+          <p>For the past 10 years I've owned a Digital Marketing business, and have learned HTML and CSS along with
+            other technologies to update my clients' websites. Owning a business requires wearing many hats, and I've
+            broadened my experience in ways I never could have imagined. </p>
+          <p>Although I've enjoyed running my business, I look forward to working with a team on a project again. I find
+            brainstorming to be invigorating and love seeing ideas get better when other people are involved. I've
+            been taking courses in Java, Javascript, React, algorithms and data structures this past year. With the knowledge
+            gained from Bootcamp, I am ready to apply myself to a project. When someone gives me an assignment, they
+            know I won't stop until I GET IT DONE. </p>
+          <p>When I'm not learning new skills, coding or working, I spend time in nature which renews my spirit. Hiking,
+            biking, kayaking are some of my favorite activities. I've also recently taken up pickleball which is a whole
+            lot of fun. </p>
+        </p>
+      </div>
+    </div>
+            <p className='px-5'>
           </p>
     </div>)
 }
--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,11 +1,99 @@
 import React from 'react';
 import './Projects.css';
+import Barker from '../../../images/Barker-homepage-cropped.png';
+import Food from '../../../images/Food-for-days.PNG';
+import Weather from '../../../images/Five-day-forecast-cropped.png';
+import Note from '../../../images/Home-page-of-Note-taker.PNG'
+import Work from '../../../images/Work-Day-Planner.png'
 
 export default function Projects() {
 	return (
-		<div class="container mt-5">
+		<div className="container mt-5">
 			<h1 className="text-end px-5 mb-5 bg-secondary text-white py-3">Projects</h1>
-			Projects
+			<div className="row card-group py-3">
+				<div className="col-sm-6">
+					<div className="card">
+						<img className="card-img-top" src={Barker} alt="Barker" height="75%" />
+						<div className="card-body d-flex justify-content-around">
+							<a href="https://stevekoutsodontis.github.io/Barker/" className="btn btn-secondary">
+								Deployed
+							</a>
+							<a href="https://github.com/SteveKoutsodontis/Barker" className="btn btn-secondary">
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6">
+					<div className="card">
+						<img className="card-img-top" src={Weather} alt="Weather Dashboard" />
+						<div className="card-body d-flex justify-content-around">
+							<a href="https://llwaterhouse.github.io/Weather-Dashboard/" className="btn btn-secondary">
+								Deployed
+							</a>
+							<a href="https://github.com/llwaterhouse/Weather-Dashboard/" className="btn btn-secondary">
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row py-3">
+				<div className="col-sm-6">
+					<div className="card border-secondary ">
+						<img className="card-img-top" src={Food} alt="Food-For-Days" />
+						<div className="card-body d-flex justify-content-around">
+							<a href="https://food-for-days.herokuapp.com/" className="btn btn-secondary">
+								Deployed
+							</a>
+							<a href="https://github.com/quynhndinh/Food-For-Days" className="btn btn-secondary">
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6">
+					<div className="card">
+						<img className="card-img-top" src={Note} alt="Notetaker" />
+						<div className="card-body d-flex justify-content-around">
+							<a href="https://notetaker-powered-by-express.herokuapp.com/" className="btn btn-secondary">
+								Deployed
+							</a>
+							<a href="https://github.com/llwaterhouse/NoteTaker-Powered-by-Express" className="btn btn-secondary">
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-sm-6">
+					<div className="card">
+						<img className="card-img-top" src={Work} alt="Work Day Planner" />
+						<div className="card-body d-flex justify-content-around">
+							<a href="https://llwaterhouse.github.io/Work-Day-Planner/" className="btn btn-secondary">
+								Deployed
+							</a>
+							<a href="https://github.com/llwaterhouse/Work-Day-Planner" className="btn btn-secondary">
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6">
+					<div className="card">
+						<img className="card-img-top" src={Barker} alt="Barker" />
+						<div className="card-body d-flex justify-content-around">
+							<a href="https://stevekoutsodontis.github.io/Barker/" className="btn btn-secondary">
+								Deployed
+							</a>
+							<a href="https://github.com/SteveKoutsodontis/Barker" className="btn btn-secondary">
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
